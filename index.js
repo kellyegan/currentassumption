9a91caba7bb0687d581f7ebc47dc2bfc7675d095
Kelly Egan <kelly.egan@gmail.com>
2017-03-13 15:15:32 -0400

    Serve a static site via express

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 /*jslint node:true */
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
 
-var server = http.createServer(function (req, res) {
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
   'use strict';
-
-  if (req.url === "/") {
-    fs.readFile("./public/index.html", "UTF-8", function (err, html) {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end(html);
-    });
-  } else {
-    res.writeHead(404, {"Content-Type": "text/html"});
-    res.end("File not found");
-  }
-
+  console.log("Current assumption site listening on port 3000");
 });
-
-server.listen(3000);
-
