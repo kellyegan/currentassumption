4bb3d534278511d36479b8d934b90023aea957fe
Kelly Egan <kelly.egan@gmail.com>
2017-03-15 17:02:12 -0400

    Add logging of requests

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 var express = require('express');
 var app = express();
 
+app.use( function (request, response, next) {
+  console.log(`${request.method} request for '${request.url}'`);
+  next();
+});
+
 app.use(express.static('public'));
 
 app.listen(3000, function () {
