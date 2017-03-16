85f4808d4604148ee6d5ea8e415398d832092121
Kelly Egan <kelly.egan@gmail.com>
2017-03-15 21:37:50 -0400

    Add /assumptions route to serve JSON

--- index.js
+++ index.js
@@ -3,12 +3,29 @@
 var express = require('express');
 var app = express();
 
-app.use( function (request, response, next) {
+var assumptions = [
+  {
+    assumption: "Change is inevitable.",
+    date: "2017-03-16T00:48:08.307Z",
+    author: "kellyegan"
+  },
+  {
+    assumption: "Uncertainty is certain.",
+    date: "2017-03-16T00:50:01.706Z",
+    author: "kellyegan"
+  }
+];
+
+app.use(function (request, response, next) {
   console.log(`${request.method} request for '${request.url}'`);
   next();
 });
 
 app.use(express.static('public'));
+
+app.get("/assumptions", function (request, response) {
+  response.json(assumptions);
+});
 
 app.listen(3000, function () {
   'use strict';
