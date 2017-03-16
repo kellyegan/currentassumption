e1cfd5d8f75febd14794abfa26f3076053318f76
Kelly Egan <kelly.egan@gmail.com>
2017-03-16 11:58:54 -0400

    Add jquery animation to transition between list of assumptions.

---
+++ public/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $.getJSON('/assumptions', function (assumptions) {
+    nextAssumption(assumptions, 0);
+  });
+});
+
+//Increment assumptions array and show next assumption
+function nextAssumption(assumptions, index) {
+  index = (index + 1) % assumptions.length;
+  $(".current-assumption").fadeOut(1500, function (){
+    setAssumption( assumptions[index], $(this) );
+
+    $(this).fadeIn(1200).delay(6000);
+    nextAssumption( assumptions, index );
+  });
+}
+
+//Set assumption text from assumption objects
+function setAssumption(assumption, element) {
+  var d = new Date(assumption.date);
+  element.children(".content").text(assumption.assumption);
+  //element.children(".date").text(d.toLocaleDateString());
+  element.children(".date").text(formatDate( d ));
+}
+
+//Format the date
+function formatDate( d ) {
+  d = new Date();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return months[d.getMonth()] + " " + d.getFullYear();
+}
+
+
