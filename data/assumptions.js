8251086e8012d26b7c1488f312b9bf420474b14c
Kelly Egan <kelly.egan@gmail.com>
2017-03-17 16:51:21 -0400

    Move data to separate module

---
+++ data/assumptions.js
@@ -0,0 +1,12 @@
+exports.data = [
+  {
+    date: "2017-03-16T00:48:08.307Z",
+    content: "Change is inevitable.",
+    author: "kellyegan"
+  },
+  {
+    date: "2017-03-16T00:50:01.706Z",
+    content: "Uncertainty is certain.",
+    author: "kellyegan"
+  }
+];
--- index.js
+++ index.js
@@ -2,20 +2,10 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var assumptions = require("./data/assumptions").data;
+
 var app = express();
 
-var assumptions = [
-  {
-    date: "2017-03-16T00:48:08.307Z",
-    content: "Change is inevitable.",
-    author: "kellyegan"
-  },
-  {
-    date: "2017-03-16T00:50:01.706Z",
-    content: "Uncertainty is certain.",
-    author: "kellyegan"
-  }
-];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
