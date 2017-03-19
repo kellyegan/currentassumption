e7f2a3c35f96fbc10ed696bb629f076ce575ed9f
Kelly Egan <kelly.egan@gmail.com>
2017-03-19 14:16:51 -0400

    Add another test assumption

--- data/assumptions.js
+++ data/assumptions.js
@@ -5,6 +5,11 @@ exports.data = [
     author: "kellyegan"
   },
   {
+    date: "2017-03-19T17:53:37.104Z",
+    content: "Democracy, however fleeting is the most stable form of government.",
+    author: "kellyegan"
+  },
+  {
     date: "2017-03-16T00:50:01.706Z",
     content: "Uncertainty is certain.",
     author: "kellyegan"
